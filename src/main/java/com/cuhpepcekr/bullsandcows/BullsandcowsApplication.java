ba8e2dbc389f5cad60697665bbfa8c0cf2652d17
clover <lintogm@gmail.com>
2020-07-07 20:47:02 +0900

    init

---
+++ src/main/java/com/cuhpepcekr/bullsandcows/BullsandcowsApplication.java
@@ -0,0 +1,13 @@
+package com.cuhpepcekr.bullsandcows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullsandcowsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BullsandcowsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cuhpepcekr/bullsandcows/BullsandcowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuhpepcekr.bullsandcows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BullsandcowsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
